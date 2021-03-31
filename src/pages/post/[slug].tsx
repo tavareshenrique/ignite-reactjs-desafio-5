ac866e021f4d02b36be66571e5841ef4b6af80f3
Henrique Tavares <ihenrits@gmail.com>
2021-03-31 15:15:22 -0300

    adicionando utteranc

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -105,6 +105,28 @@ export default function Post({ post }: PostProps) {
             </article>
           ))}
         </main>
+
+        <footer className={`${commonStyles.content} ${styles.postFooter}`}>
+          <section
+            ref={elem => {
+              if (!elem) {
+                return;
+              }
+              const scriptElem = document.createElement('script');
+              scriptElem.src = 'https://utteranc.es/client.js';
+              scriptElem.async = true;
+              scriptElem.crossOrigin = 'anonymous';
+              scriptElem.setAttribute(
+                'repo',
+                'tavareshenrique/ignite-reactjs-desafio-5'
+              );
+              scriptElem.setAttribute('issue-term', 'pathname');
+              scriptElem.setAttribute('label', 'blog-comment');
+              scriptElem.setAttribute('theme', 'dark-blue');
+              elem.appendChild(scriptElem);
+            }}
+          />
+        </footer>
       </div>
     </>
   );
